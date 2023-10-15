b9582ac3735e9c1c85799d77db2c112e4cdfa0f5
sinhhung <20110647@student.hcmute.edu.vn>
2023-10-15 14:01:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+const app = express();
+// middleware
+
+app.use(express.json());
+
+// routes
+
+app.use('/api/v1/tasks', tasks);
+
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
